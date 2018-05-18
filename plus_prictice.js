c92280b492582ad919ae0720bc88b4d236c72cc0
monkey_code <31238244+testner@users.noreply.github.com>
2018-05-18 18:21:26 +0800

    tiaojiao

---
+++ plus_prictice.js
@@ -0,0 +1,10 @@
+
+
+var sNum = "20";
+alert(typeof sNum); //输出 "string"
+var iNum = +sNum;
+//alert(typeof iNum); //输出 "number"
+console.log(typeof sNum);
+
+console.log(object.protype.tostring.call(sNum));
+
